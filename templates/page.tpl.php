748927e4288b3d5eaa87d80978d8d4917e5fe8b2
camp5456 <tcamp@ou.edu>
2014-04-10 10:13:04 -0500

    change toggle menu
    carl doesn't like the three bars icon for the toggle menu. I changes it
    to the word menu.

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -120,9 +120,10 @@
       <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
       <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only">Toggle navigation</span>
+        <!-- <span class="icon-bar"></span>
         <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> -->
+        <p class="small">MENU</p>
       </button>
     </div>
 
